60771d0651d95cb5222394a6fbebce0e973a6696
cxj <cscxjjj@gmail.com>
2025-12-17 16:36:01 +0800

    saev

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 function main() {
     console.log("Hello World");
+
+    console.log(document.getElementById("app").innerHTML);
+    
+    console.log(require("os").platform());
+
+    System.out.println("Hello World");
 }
 
 
