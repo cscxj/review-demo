79739904e4cb48fcd4b7d6d6a37889ae867db1d4
cxj <cscxjjj@gmail.com>
2025-12-17 16:24:29 +0800

    aaaa

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
+const { log } = require("console");
+
 function main() {
   console.log("Hello World");
 
   console.log("Hello World2");
 
   console.log(require("os").platform());
+
+  console.log(document.getElementById("app").innerHTML);
 }
 
 main();
